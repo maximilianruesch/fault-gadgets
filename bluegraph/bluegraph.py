f3186506471ce1c5082b19e08eae648bc844dc10
Maximilian Rüsch <xammaximus.ruesch@gmail.com>
2025-03-23 16:25:08 +0000

    Allow pushing simple targets

--- bluegraph/bluegraph.py
+++ bluegraph/bluegraph.py
@@ -1,6 +1,6 @@
 from typing import Tuple, Dict, Iterable, Literal
 
-from .dongles import Dongle, XTarget, ZTarget, DongleTarget, DongleTargetType
+from .dongles import Dongle, DongleTarget, DongleTargetType
 from pyzx import EdgeType, VertexType
 from pyzx.graph.graph_s import GraphS
 
@@ -8,12 +8,16 @@ class BlueGraph(GraphS):
     def __init__(self) -> None:
         GraphS.__init__(self)
         self._blue: Dict[Tuple[int, int], bool] = dict()
-        self._dongles: Dict[int, Dongle] = dict()
+        self._targets: Dict[int, DongleTarget] = dict() # ID (main node) -> Target
+        self._distributors: Dict[int, int] = dict() # ID (main node) -> Distributor
+        self._realized: Dict[int, bool] = dict() # ID (main node) -> 'realized' as bool value
 
     def clone(self):
         cpy = GraphS.clone(self)
         cpy._blue = self._blue.copy()
-        cpy._dongles = self._dongles.copy()
+        cpy._targets = self._targets.copy()
+        cpy._distributors = self._distributors.copy()
+        cpy._realized = self._realized.copy()
 
         return cpy
 
@@ -25,7 +29,35 @@ class BlueGraph(GraphS):
     def _is_blue(self, edge: Tuple[int, int]) -> bool:
         return self._blue[edge]
 
-    def add_dongles(self, edge: Tuple[int, int], repack=False) -> Tuple[Dongle, Dongle]:
+    def _local_target_info(self, id_node: int) -> Tuple[int, int, int]:
+        """
+        :param id_node: The id of the target to fetch local info for
+        :return: The next distributor + 'left' and 'right' adjacent nodes
+        """
+        dist = self._distributors[id_node]
+        adjacent_nodes = [
+            e1 if e2 == id_node else e2
+            for e1, e2, in self.edges(id_node)
+        ]
+        if len(adjacent_nodes) != 3:
+            raise ValueError(f"The given node {id_node} is not adjacent to exactly 3 other nodes!")
+        elif not adjacent_nodes.__contains__(dist):
+            raise RuntimeError(f"The given node {id_node} is not adjacent to its distributor!")
+        adjacent_nodes.remove(dist)
+        return dist, adjacent_nodes[0], adjacent_nodes[1]
+
+    def remove_edge(self, edge):
+        GraphS.remove_edge(self, edge)
+        if self._blue.__contains__(edge):
+            del self._blue[edge]
+
+    def remove_edges(self, edges):
+        GraphS.remove_edges(self, edges)
+        for edge in edges:
+            if self._blue.__contains__(edge):
+                del self._blue[edge]
+
+    def add_dongles(self, edge: Tuple[int, int], repack=False) -> Tuple[Dongle, Dongle, Dongle]:
         edge_type = self.edge_type(edge)
         if edge_type == 0:
             raise ValueError('Edge to convert is not in graph!')
@@ -36,11 +68,17 @@ class BlueGraph(GraphS):
 
         s,t = edge
 
-        z_dongle, z_left, z_right = self._z_dongle()
-        x_dongle, x_left, x_right = self._x_dongle()
-        y_dongle, y_left, y_right = self._y_dongle()
+        z_dongle, z_target = self._z_dongle()
+        x_dongle, x_target = self._x_dongle()
+        y_dongle, y_target_1, y_target_2 = self._y_dongle()
 
-        blue_edges = [(s, z_left), (z_right, x_left), (x_right, y_left), (y_right, t)]
+        blue_edges = [
+            (s, z_target),
+            (z_target, x_target),
+            (x_target, y_target_1),
+            (y_target_1, y_target_2),
+            (y_target_2, t),
+        ]
         self.add_edges(blue_edges, edgetype=EdgeType.SIMPLE)
         self._mark_blue(blue_edges)
 
@@ -49,19 +87,19 @@ class BlueGraph(GraphS):
 
         assert self.is_well_formed(), "Circuit is not well formed anymore!"
 
-        return x_dongle, z_dongle
+        return x_dongle, z_dongle, y_dongle
 
-    def _x_dongle(self) -> Tuple[Dongle, int, int]:
+    def _x_dongle(self) -> Tuple[Dongle, int]:
         dongle = self._instantiate_dongle(types=['X'])
-        return dongle, dongle.targets[0].get_left(), dongle.targets[0].get_right()
+        return dongle, dongle.targets[0].get_id()
 
-    def _z_dongle(self) -> Tuple[Dongle, int, int]:
+    def _z_dongle(self) -> Tuple[Dongle, int]:
         dongle = self._instantiate_dongle(types=['Z'])
-        return dongle, dongle.targets[0].get_left(), dongle.targets[0].get_right()
+        return dongle, dongle.targets[0].get_id()
 
     def _y_dongle(self) -> Tuple[Dongle, int, int]:
         dongle = self._instantiate_dongle(types=['Y'])
-        return dongle, dongle.targets[0].get_left(), dongle.targets[1].get_right()
+        return dongle, dongle.targets[0].get_id(), dongle.targets[1].get_id()
 
     def _instantiate_dongle(self, types: Iterable[Literal['X', 'Y', 'Z']]):
         spawn = self.add_vertex(VertexType.Z, qubit=-4, row=1.2)
@@ -71,54 +109,104 @@ class BlueGraph(GraphS):
         blue_edges = [(spawn, dist)]
         targets = []
 
-        def _x_target(local_distributor: int) -> XTarget:
-            hadamard_left = self.add_vertex(VertexType.H_BOX, qubit=-1, row=1.7)
-            node = self.add_vertex(VertexType.Z, qubit=-1, row=1.8)
-            hadamard_right = self.add_vertex(VertexType.H_BOX, qubit=-1, row=1.9)
-            blue_edges.extend([
-                (local_distributor, node),
-                (hadamard_left, node),
-                (node, hadamard_right),
-            ])
-            return XTarget(node, hadamard_left, hadamard_right)
-
-        def _z_target(local_distributor: int) -> ZTarget:
-            node = self.add_vertex(VertexType.Z, qubit=-1, row=1.6)
-            blue_edges.append((local_distributor, node))
-            return ZTarget(node)
-
         for target_type in types:
             if target_type == 'X':
-                targets.append(_x_target(dist))
+                targets.append(self._add_target(DongleTargetType.X, dist))
             elif target_type == 'Z':
-                targets.append(_z_target(dist))
+                targets.append(self._add_target(DongleTargetType.Z, dist))
             else:
                 local_dist = self.add_vertex(VertexType.X, qubit=-2, row=1.5)
-                z_target = _z_target(local_dist)
-                x_target = _x_target(local_dist)
-                blue_edges.extend([
-                    (dist, local_dist),
-                    (local_dist, z_target.node),
-                    (local_dist, x_target.node),
-                    (z_target.node, x_target.hadamard_left)
-                ])
-                targets.extend([z_target, x_target])
+                targets.append(self._add_target(DongleTargetType.X, local_dist))
+                targets.append(self._add_target(DongleTargetType.Z, local_dist))
+                blue_edges.append((dist, local_dist))
 
         self.add_edges(blue_edges, edgetype=EdgeType.SIMPLE)
         self._mark_blue(blue_edges)
-        dongle = Dongle(self, spawn, dist, targets)
-        self._dongles[spawn] = dongle
 
-        return dongle
+        return Dongle(self, spawn, dist, targets)
 
-    def push_target(self, target: DongleTarget, gateway: int) -> None:
-        """ Push the target through the specified node given by the gateway id """
+    def _add_target(self, _type: DongleTargetType, local_distributor: int):
+        # TODO choose a more appropriate node type
+        _id_node = self.add_vertex(VertexType.Z_BOX, qubit=-1, row=1.7)
+        _target = DongleTarget(
+            _id=_id_node,
+            target_type=_type
+        )
+        self._targets[_id_node] = _target
+        self._distributors[_id_node] = local_distributor
+        self._realized[_id_node] = False
+        self.add_edge((local_distributor, _id_node), edgetype=EdgeType.SIMPLE)
+        self._mark_blue([(local_distributor, _id_node)])
+
+        return _target
+
+    def realise_all_targets(self) -> None:
+        for id_node, target in self._targets.items():
+            ntype = target.get_target_type()
+            dist, adj_left, adj_right = self._local_target_info(id_node)
+
+            edges_to_add = []
+            if ntype == DongleTargetType.X:
+                hadamard_left = self.add_vertex(VertexType.H_BOX, qubit=-1, row=1.7)
+                new_node = self.add_vertex(VertexType.Z, qubit=-1, row=1.8)
+                hadamard_right = self.add_vertex(VertexType.H_BOX, qubit=-1, row=1.9)
+                edges_to_add.extend([
+                    (dist, new_node),
+                    (hadamard_left, new_node),
+                    (new_node, hadamard_right),
+                    (adj_left, hadamard_left),
+                    (adj_right, hadamard_right),
+                ])
+            elif ntype == DongleTargetType.Z:
+                new_node = self.add_vertex(VertexType.Z, qubit=-1, row=1.6)
+                edges_to_add.extend([
+                    (dist, new_node),
+                    (adj_left, new_node),
+                    (adj_right, new_node),
+                ])
+            else:
+                raise RuntimeError(f"Unexpected blue target type: {ntype}")
+            self.remove_vertex(id_node)
+            self.add_edges(edges_to_add)
+            self._mark_blue(edges_to_add)
+            self._realized[id_node] = True
+
+    def push_target(self, target: DongleTarget, new_edge: Tuple[int, int]) -> None:
+        """
+        Push the target to the next edge which must be adjacent.
+        May have side effects / introduce / remove dongles.
+        """
+        id_node = target.get_id()
+        if self._realized[id_node]: raise ValueError("Target already realized, cannot push!")
+        dist, s, t = self._local_target_info(id_node)
+
+        common_nodes = {s,t}.intersection(new_edge)
+        if len(common_nodes) == 2:
+            raise ValueError(f"Given target is already on the edge: {new_edge}!")
+        elif len(common_nodes) == 0:
+            raise ValueError(f"Given edge {new_edge} is not adjacent to dongle target edge {(s,t)}!")
+
+        gateway = common_nodes.pop()
         gateway_type = self.type(gateway)
         if gateway_type == VertexType.Z:
-            pass # TODO
+            if target.get_target_type() == DongleTargetType.Z:
+                # Simply push through
+                self.remove_edges([new_edge, (s, id_node), (t, id_node)])
+                added_edges = [(s, t), (new_edge[1], id_node), (new_edge[0], id_node)]
+                self.add_edges(added_edges)
+                self._mark_blue(added_edges)
+            else:
+                raise NotImplementedError("Cannot push X target through Z node for now!")
         elif gateway_type == VertexType.X:
-            pass # TODO
+            if target.get_target_type() == DongleTargetType.X:
+                # Simply push through
+                self.remove_edges([new_edge, (s, id_node), (t, id_node)])
+                added_edges = [(s, t), (new_edge[1], id_node), (new_edge[0], id_node)]
+                self.add_edges(added_edges)
+                self._mark_blue(added_edges)
+            else:
+                raise NotImplementedError("Cannot push Z target through X node for now!")
         elif gateway_type == VertexType.H_BOX:
             pass # TODO (also what if this has multiple legs?
         else:
-            raise NotImplementedError(f"Gateway type {gateway_type.name} unhandled right now")
+            raise NotImplementedError(f"Gateway type {gateway_type.name} unhandled right now!")
--- bluegraph/dongles.py
+++ bluegraph/dongles.py
@@ -8,49 +8,18 @@ class DongleTargetType(IntEnum):
     Z = 2
 
 class DongleTarget:
+    _id: int
     _target_type: DongleTargetType
 
-    def __init__(self, target_type: DongleTargetType):
+    def __init__(self, _id: int, target_type: DongleTargetType):
+        self._id = _id
         self._target_type = target_type
+
+    def get_id(self) -> int:
+        return self._id
 
     def get_target_type(self) -> DongleTargetType:
         return self._target_type
-
-    def get_left(self) -> int:
-        raise NotImplementedError("Needs to be implemented in a subclass!")
-
-    def get_right(self) -> int:
-        raise NotImplementedError("Needs to be implemented in a subclass!")
-
-class XTarget(DongleTarget):
-    node: int
-    hadamard_left: int
-    hadamard_right: int
-
-    def __init__(self, node: int,hadamard_left: int, hadamard_right: int):
-        DongleTarget.__init__(self, DongleTargetType.X)
-        self.node = node
-        self.hadamard_left = hadamard_left
-        self.hadamard_right = hadamard_right
-
-    def get_left(self) -> int:
-        return self.hadamard_left
-
-    def get_right(self) -> int:
-        return self.hadamard_right
-
-class ZTarget(DongleTarget):
-    node: int
-
-    def __init__(self, node: int):
-        DongleTarget.__init__(self, DongleTargetType.Z)
-        self.node = node
-
-    def get_left(self) -> int:
-        return self.node
-
-    def get_right(self) -> int:
-        return self.node
 
 class Dongle:
     graph: GraphS
