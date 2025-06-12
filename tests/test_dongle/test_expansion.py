1bd30368aaea3e8d37cd8492547713036a6d4a94
Maximilian Rüsch <xammaximus.ruesch@gmail.com>
2025-06-12 10:01:22 +0100

    Fix flaky tests

--- tests/test_dongle/test_expansion.py
+++ tests/test_dongle/test_expansion.py
@@ -1,6 +1,5 @@
 import pytest
 
-from pyzx import compare_tensors, id_simp, bialg_simp
 from pyzx.graph.base import BaseGraph
 from pyzx.hsimplify import from_hypergraph_form
 
@@ -14,8 +13,17 @@ def verbosity_level(request):
     return request.config.option.verbose
 
 def _dongle_simp(dg: DongleGraph) -> None:
-    bialg_simp(dg)
-    id_simp(dg)
+    matches = [
+        (dongle.spawn, dongle.dist, [], [target.id for target in dongle.targets])
+        for dongle in dg.dongles().values()
+    ]
+    etab, rem_vertices, rem_edges, check_isolated_vertices = zx.rules.bialg(dg, matches)
+    dg.add_edge_table(etab)
+    dg.remove_edges(rem_edges)
+    dg.remove_vertices(rem_vertices)
+    if check_isolated_vertices: dg.remove_isolated_vertices()
+
+    zx.id_simp(dg)
 
 def _assert_circuit_equality(g: BaseGraph, dg: DongleGraph, verbosity_level) -> None:
     dg_cp = dg.clone(DongleGraph())
@@ -28,7 +36,7 @@ def _assert_circuit_equality(g: BaseGraph, dg: DongleGraph, verbosity_level) -> 
     if verbosity_level > 1:
         print(f"Reduced dongle graph from {num_v}:{num_e} to {dg_cp.num_vertices()}:{dg_cp.num_edges()}")
 
-    assert compare_tensors(g, dg_cp, preserve_scalar=False)
+    assert zx.compare_tensors(g, dg_cp)
 
 @pytest.mark.parametrize("qubits,depth", [(2, 2), (4, 5)])
 def test_cnot(qubits, depth, verbosity_level):
