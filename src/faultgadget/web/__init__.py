250a11cfb10b5ac90fddccbbe5fc181a41c3c644
Maximilian Rüsch <xammaximus.ruesch@gmail.com>
2025-06-27 13:45:40 +0100

    Add computation of stabilisers via Pauli Webs

--- src/faultgadget/web/__init__.py
+++ src/faultgadget/web/__init__.py
@@ -1,4 +1,4 @@
 from .pauli import Pauli, PauliWeb
 from .graphlike import to_red_green_graphlike
 from .firing_assignments import determine_ordering, create_firing_verification, convert_firing_assignment_to_web
-from .compute import compute_webs, compute_detecting_regions
+from .compute import compute_webs, compute_detecting_regions, compute_stabilisers
--- src/faultgadget/web/compute.py
+++ src/faultgadget/web/compute.py
@@ -1,5 +1,7 @@
 from typing import List
 
+import numpy as np
+
 from pyzx import Mat2
 from pyzx.graph.graph_s import GraphS
 from .graphlike import to_red_green_graphlike
@@ -39,6 +41,27 @@ def compute_detecting_regions(graph: GraphS) -> List[PauliWeb]:
 
     region_sols = (Mat2(boundary_nullspace_vectors) * sol_basis.transpose()).data
     webs = list(map(lambda v: convert_firing_assignment_to_web(g, ordering, v), region_sols))
+    for web in webs: additional_nodes.remove_from(g, web)
+
+    return webs
+
+def compute_stabilisers(graph: GraphS) -> List[PauliWeb]:
+    g = graph.clone()
+
+    additional_nodes = to_red_green_graphlike(g)
+    ordering = determine_ordering(g)
+    m_d = create_firing_verification(g, ordering)
+
+    # Compute basis of valid firing assignment space
+    sol_basis = Mat2(m_d.nullspace())
+    # Search for solutions that do not highlight boundary edges, i.e. detecting regions
+    boundary_selected_basis = sol_basis.transpose()[:len(ordering.z_boundaries) * 2,:]
+
+    pivot_cols = []
+    boundary_selected_basis.gauss(pivot_cols=pivot_cols)
+    stabilisers = [sol_basis.data[i] for i in pivot_cols]
+
+    webs = list(map(lambda v: convert_firing_assignment_to_web(g, ordering, v), stabilisers))
     for web in webs: additional_nodes.remove_from(g, web)
 
     return webs
