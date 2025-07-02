85bd16aced2c3e1c5f8366acde64b3d849f12b8a
Maximilian Rüsch <xammaximus.ruesch@gmail.com>
2025-07-02 10:02:33 +0100

    Add cat state test for distance preservation

--- tests/test_query/test_distance_preservation.py
+++ tests/test_query/test_distance_preservation.py
@@ -1,5 +1,5 @@
 import math
-from typing import List
+from typing import List, Tuple
 
 import pytest
 
@@ -72,3 +72,30 @@ def test_collapse_ring(ring_size):
         assert is_distance_preserving(g1, g2)
     else:
         assert not is_distance_preserving(g1, g2)
+
+def _add_cat_state(g: GraphS, size: int) -> Tuple[int, List[int]]:
+    z = g.add_vertex(zx.VertexType.Z)
+    boundaries = [g.add_vertex(zx.VertexType.BOUNDARY) for _ in range(size)]
+    g.add_edges([(z, b) for b in boundaries])
+
+    return z, boundaries
+
+@pytest.mark.parametrize("n", [2, 3, 4, 5])
+def test_cat_state_decomposition(n):
+    """
+    Expanding a cat state with 2n legs into two cat states with n legs.
+    From https://arxiv.org/pdf/2506.17181.
+    """
+    g1 = GraphS()
+    _add_cat_state(g1, size=2*n)
+
+    g2 = GraphS()
+    _, bs1 = _add_cat_state(g2, size=n)
+    _, bs2 = _add_cat_state(g2, size=n)
+    new_bs = [g2.add_vertex(zx.VertexType.BOUNDARY) for _ in range(2*n)]
+    for i in range(n):
+        g2.set_type(bs1[i], zx.VertexType.Z)
+        g2.set_type(bs2[i], zx.VertexType.Z)
+        g2.add_edges([(bs1[i], bs2[i]), (bs1[i], new_bs[i]), [bs2[i], new_bs[i + n]]])
+
+    assert is_distance_preserving(g1, g2, quiet=False)
