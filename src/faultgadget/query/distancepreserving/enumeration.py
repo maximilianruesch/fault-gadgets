6ddbeb1777066eda382a98abd0e8d1b9db9f585c
Maximilian Rüsch <xammaximus.ruesch@gmail.com>
2025-07-05 20:03:51 +0100

    Add progress bars

--- src/faultgadget/query/distancepreserving/enumeration.py
+++ src/faultgadget/query/distancepreserving/enumeration.py
@@ -1,4 +1,6 @@
 import itertools
+from tqdm.auto import tqdm
+import time
 from typing import List, Optional
 
 import numpy as np
@@ -53,9 +55,11 @@ def _smallest_size_iteration(g1_sig_nf: List[GF2], g2_sig_nf: List[GF2],
     if not quiet: print(f"Starting iteration until {len(g2_sig_nf)}!")
     g1_last_new_signatures = [GF2.Zeros(g1_sig_nf[0].shape)]
     g2_last_new_detectable = [GF2.Zeros(g2_sig_nf[0].shape)]
-    for max_size in range(1, len(g2_sig_nf) + 1):
-        if not quiet: print(f"Starting iteration with combinatory size: {max_size}...")
+    for max_size in tqdm(range(1, len(g2_sig_nf) + 1),
+                        desc='Weight: ', initial=1, total=len(g2_sig_nf), leave=False, disable=quiet):
+        if not quiet: tqdm.write(f"Starting iteration with combinatory size: {max_size}...")
         # Populate g1_lookup for this weight
+        g1_time = time.time()
         g1_new_signatures = []
         for last_it, atomic_sig_nf in itertools.product(g1_last_new_signatures, g1_sig_nf):
             combined_sig = last_it + atomic_sig_nf
@@ -67,11 +71,15 @@ def _smallest_size_iteration(g1_sig_nf: List[GF2], g2_sig_nf: List[GF2],
                 g1_new_signatures.append(combined_sig)
                 g1_lookup[sig_int] = max_size
         g1_last_new_signatures = g1_new_signatures
+        if not quiet: tqdm.write(f"Populating g1 lookup took {time.time() - g1_time}s.")
 
         # Incrementally discover g2 signatures by combining #`max_size` atomic signatures
+        g2_time = time.time()
         num_new_signatures = 0
         g2_new_detectable = []
-        for last_it, atomic_sig_nf in itertools.product(g2_last_new_detectable, g2_sig_nf):
+        for last_it, atomic_sig_nf in tqdm(itertools.product(g2_last_new_detectable, g2_sig_nf),
+                                        desc='Signatures: ', total=len(g2_last_new_detectable) * len(g2_sig_nf),
+                                        leave=False, disable=quiet):
             combined_sig = last_it + atomic_sig_nf
             sig_int = _sig_to_int(combined_sig)
             if sig_int in g2_lookup:
@@ -85,16 +93,16 @@ def _smallest_size_iteration(g1_sig_nf: List[GF2], g2_sig_nf: List[GF2],
 
             # Perform search with real output signature
             if _sig_wo_sinks_to_int(combined_sig, g2_sinks) not in g1_lookup:
-                if not quiet: print(f"{_format_sig(combined_sig, g2_sinks)} has no equivalent in g1, or it was not yet generated and thus has higher weight!")
+                if not quiet: tqdm.write(f"{_format_sig(combined_sig, g2_sinks)} has no equivalent in g1, or it was not yet generated and thus has higher weight!")
                 return max_size # No equivalent error with equal or lower weight found
+        if not quiet: tqdm.write(f"Populating g2 lookup took {time.time() - g2_time}s.")
 
         g2_last_new_detectable = g2_new_detectable
         num_total_signatures += num_new_signatures
         if num_new_signatures == 0:
-            if not quiet: print("No new signatures discovered!")
+            if not quiet: tqdm.write("No new signatures discovered!")
             break
         else:
-            if not quiet: print(f"Discovered {num_new_signatures} new signatures this iteration (so far: {num_total_signatures}, max signatures: {num_max_signatures})!.")
-
+            if not quiet: tqdm.write(f"Discovered {num_new_signatures} new signatures this iteration (so far: {num_total_signatures}, max signatures: {num_max_signatures})!.")
 
     return None
