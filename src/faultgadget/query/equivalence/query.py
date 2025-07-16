692c51dd2ef33641573f02264f6f5819b1e3251b
Maximilian Rüsch <xammaximus.ruesch@gmail.com>
2025-07-16 16:28:45 +0100

    Calculate signature normal forms in bulk

--- src/faultgadget/query/equivalence/query.py
+++ src/faultgadget/query/equivalence/query.py
@@ -39,20 +39,31 @@ def _index_graph_sinks(g: GadgetGraph) -> Tuple[Mapping[int, int], int]:
 
     return sink_id_to_idx, num_sinks
 
+class Stabilisers:
+    def __init__(self, stabiliser_rref: GF2):
+        self._rref = stabiliser_rref
+        self._indices = self._rref.argmax(axis=1).view(np.ndarray)
+
 class AugmentedStabilisers:
-    def __init__(self, stabiliser_rref: GF2, num_sinks: int):
-        self._rref = GF2(np.hstack([stabiliser_rref, GF2.Zeros((len(stabiliser_rref), num_sinks))]))
-        self._indices = np.argmax(self._rref, axis=1).view(np.ndarray)
-        self._rref_and_indices = [(stab, idx) for stab, idx in zip(self._rref, self._indices) if stab[idx] == 1]
+    _rref: GF2
+    _indices: np.ndarray
 
-    def normalise_signature(self, sig: GF2) -> GF2: # TODO bulk normalise signatures
-        normalised_sig = sig
-        for stab, idx in self._rref_and_indices:
-            if sig[idx] == 1:
-                normalised_sig += stab
-        return normalised_sig
+    @staticmethod
+    def from_stabilisers(stabilisers: Stabilisers, num_sinks: int) -> 'AugmentedStabilisers':
+        self = AugmentedStabilisers()
+        self._rref = GF2(np.hstack([stabilisers._rref, GF2.Zeros((len(stabilisers._rref), num_sinks))]))
+        self._indices = stabilisers._indices
 
-def _stabiliser_rref(stabilisers: List[Signature], boundaries_to_idx: Mapping[int, int]) -> GF2:
+        return self
+
+    def normalise_signature(self, sig: GF2) -> GF2: # TODO remove
+        return self.normalise_signatures(GF2([sig]))[0]
+
+    def normalise_signatures(self, sigs: GF2) -> GF2:
+        return sigs + sigs[:, self._indices] @ self._rref
+
+def _stabilisers(g: GraphS, boundaries_to_idx: Mapping[int, int]) -> Stabilisers:
+    stabilisers = compute_stabiliser_signatures(g)  # TODO if strict, compute stabilisers of g2 and assert space equality
     num_boundaries = len(boundaries_to_idx)
     np_stabilisers = np.zeros((len(stabilisers), num_boundaries * 2), dtype=int)
     for i, stab in enumerate(stabilisers):
@@ -61,7 +72,7 @@ def _stabiliser_rref(stabilisers: List[Signature], boundaries_to_idx: Mapping[in
             if pauli == Pauli.Z or pauli == Pauli.Y: np_stabilisers[i, idx] = 1
             if pauli == Pauli.X or pauli == Pauli.Y: np_stabilisers[i, idx + num_boundaries] = 1
 
-    return GF2(np_stabilisers).row_reduce(eye='left')
+    return Stabilisers(GF2(np_stabilisers).row_reduce(eye='left'))
 
 def _calculate_signature_normal_forms(signatures: Mapping[int, GadgetSignature], stabs: AugmentedStabilisers,
                                       boundaries_to_idx: Mapping[int, int], num_boundaries: int,
@@ -89,7 +100,7 @@ def _add_gadgets(dg: GadgetGraph) -> None:
 
         dg.add_edge_flip_gadgets(edge)
 
-def _construct_signatures(g: GraphS, stabiliser_rref: GF2, boundaries_to_idx: Mapping[int, int],
+def _construct_signatures(g: GraphS, stabilisers: Stabilisers, boundaries_to_idx: Mapping[int, int],
                           num_boundaries: int) -> Tuple[AugmentedStabilisers, List[GF2], int]:
     gadget_graph = GadgetGraph.from_graph(g)
     add_sinks_for_all_detecting_regions(gadget_graph)
@@ -100,24 +111,27 @@ def _construct_signatures(g: GraphS, stabiliser_rref: GF2, boundaries_to_idx: Ma
     # Remove trivial signatures # TODO remove once somewhere...
     signatures = { _id: signature for _id, signature in signatures.items() if not signature.is_trivial() }
 
-    stabs = AugmentedStabilisers(stabiliser_rref, num_sinks)
+    stabs = AugmentedStabilisers.from_stabilisers(stabilisers, num_sinks)
     sig_nf = _calculate_signature_normal_forms(signatures, stabs, boundaries_to_idx, num_boundaries, sink_id_to_idx, num_sinks)
 
     return stabs, sig_nf, num_sinks
 
-def _add_boundary_signatures(stabs: AugmentedStabilisers, sig_nf: List[GF2], num_boundaries: int, num_sinks: int) -> List[GF2]:
-    augmented_sig_nf = sig_nf.copy()
+def _add_boundary_signatures(stabs: AugmentedStabilisers, sig_nf: List[GF2], num_boundaries: int, num_sinks: int) -> List[GF2]: # TODO share boundary signatures and hstack sinks on afterwards
+    sigs = []
     for i in range(num_boundaries):
         # Z Signature
         x_atomic_sig = GF2.Zeros(num_boundaries * 2 + num_sinks)
         x_atomic_sig[i] = 1
-        augmented_sig_nf.append(stabs.normalise_signature(x_atomic_sig))
+        sigs.append(x_atomic_sig)
         # X Signature
         z_atomic_sig = GF2.Zeros(num_boundaries * 2 + num_sinks)
         z_atomic_sig[i + num_boundaries] = 1
-        augmented_sig_nf.append(stabs.normalise_signature(z_atomic_sig))
+        sigs.append(z_atomic_sig)
         # Y Signature
-        augmented_sig_nf.append(stabs.normalise_signature(x_atomic_sig + z_atomic_sig))
+        sigs.append(x_atomic_sig + z_atomic_sig)
+
+    augmented_sig_nf = sig_nf.copy()
+    augmented_sig_nf.extend(stabs.normalise_signatures(GF2(sigs)))
 
     return [GF2(l) for l in np.unique(augmented_sig_nf, axis=0)]
 
@@ -131,14 +145,13 @@ def is_fault_equivalent(g1: GraphS, g2: GraphS, quiet: bool = True) -> bool:
         raise RuntimeError("Number of boundaries in graphs must be the same!")
 
     if not quiet: print("Computing stabilisers...")
-    stabilisers = compute_stabiliser_signatures(g1) # TODO if strict, compute stabilisers of g2 and assert space equality
-    stabiliser_rref = _stabiliser_rref(stabilisers, g1_boundaries_to_idx)
+    stabilisers = _stabilisers(g1, g1_boundaries_to_idx)
 
     if not quiet: print("Constructing signatures of g1...")
-    g1_stabs, g1_sig_nf, g1_num_sinks = _construct_signatures(g1, stabiliser_rref, g1_boundaries_to_idx, g1_num_boundaries)
+    g1_stabs, g1_sig_nf, g1_num_sinks = _construct_signatures(g1, stabilisers, g1_boundaries_to_idx, g1_num_boundaries)
 
     if not quiet: print("Constructing signatures of g2...")
-    g2_stabs, g2_sig_nf, g2_num_sinks = _construct_signatures(g2, stabiliser_rref, g2_boundaries_to_idx, g2_num_boundaries)
+    g2_stabs, g2_sig_nf, g2_num_sinks = _construct_signatures(g2, stabilisers, g2_boundaries_to_idx, g2_num_boundaries)
 
     if not quiet: print("Checking if g1 -> g2 is fault bounded...")
     augmented_g1_sig_nf = _add_boundary_signatures(g1_stabs, g1_sig_nf, g1_num_boundaries, g1_num_sinks)
