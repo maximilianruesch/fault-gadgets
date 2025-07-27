b45e8f7112dbf2953a6fabea36ce9a3873ae27ff
Maximilian Rüsch <xammaximus.ruesch@gmail.com>
2025-07-27 14:21:59 +0200

    Improve sampling speed using compiled signatures and reduce startup overhead

--- src/faultgadget/query/sampling/query.py
+++ src/faultgadget/query/sampling/query.py
@@ -11,15 +11,14 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from collections import defaultdict
-from typing import Dict, Iterator
+from typing import Iterator
 
 import numpy as np
 
+from pyzx import VertexType
 from pyzx.graph.graph_s import GraphS
 from ... import add_all_gadgets, GadgetGraph, add_sinks_for_all_detecting_regions, compute_signatures_for_gadgets, \
     Signature
-from ...pauli import Pauli
 
 
 def sample_signatures(g: GraphS, shots: int, p: float = 0.1) -> Iterator[Signature]:
@@ -33,30 +32,16 @@ def sample_signatures(g: GraphS, shots: int, p: float = 0.1) -> Iterator[Signatu
     add_all_gadgets(gadget_graph)
     id_to_signature = compute_signatures_for_gadgets(gadget_graph)
 
-    def combine_signatures(sig1: Signature, sig2: Signature) -> Signature:
-        boundaries: Dict[int, Pauli] = defaultdict(lambda: Pauli.I)
-        sinks: Dict[int, bool] = defaultdict(lambda: False)
+    boundaries_to_idx = { b: i for i, b in enumerate([v for v in g.vertices() if g.type(v) == VertexType.BOUNDARY])}
+    sinks_to_idx = { s: i for i, s in enumerate(gadget_graph.sinks().keys()) }
 
-        boundaries1 = set(sig1.boundaries)
-        boundaries2 = set(sig2.boundaries)
-        for b in boundaries1.intersection(boundaries2):
-            boundaries[b] = sig1.boundaries[b] * sig2.boundaries[b]
-        for b in boundaries1.difference(boundaries2):
-            boundaries[b] = sig1.boundaries[b]
-        for b in boundaries2.difference(boundaries1):
-            boundaries[b] = sig2.boundaries[b]
-
-        for s, active in sig1.sinks.items():
-            sinks[s] = active ^ (sig2.sinks.get(s) or False)
-        for s, active in sig2.sinks.items():
-            sinks[s] = active ^ (sig1.sinks.get(s) or False)
-
-        return Signature(boundaries, sinks)
-
-    for sig_active in np.random.choice(a=[False, True], size=(shots, len(id_to_signature)), p=[1 - p, p]):
-        combined_signature = Signature(dict(), dict())
+    signatures = [sig.to_int(boundaries_to_idx, sinks_to_idx) for sig in id_to_signature.values()]
+    for _ in range(shots):
+        # For adversarial noise models *specifically*, this may be improved leveraging the binomial distribution
+        sig_active = np.random.choice(a=[False, True], size=len(signatures), p=[1 - p, p])
+        combined_signature = 0
         for i, a in enumerate(sig_active):
             if a:
-                combined_signature = combine_signatures(combined_signature, id_to_signature[i])
+                combined_signature = combined_signature ^ signatures[i]
 
         yield combined_signature
